f7f78cbef4fd3096dbbe6ae86e46a84d4d62bd2f
a7xsuri <leonardofilipposurico@gmail.com>
2022-03-23 14:27:37 +0100

    add let nome, cognome, colore

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nome = prompt("Qual è il tuo nome?")
+console.log(nome)
+
+let cognome = prompt("Qual è il tuo cognome?")
+console.log(cognome)
+
+let colore = prompt("Qual è il tuo colore preferito?")
+console.log(colore)
+
+
