cbcccd482ac55fe5f677807251597dcd398257dd
a7xsuri <leonardofilipposurico@gmail.com>
2022-03-23 14:31:17 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-let nome = prompt("Qual è il tuo nome?")
-console.log(nome)
+let nome = prompt("Qual è il tuo nome?");
+console.log(nome);
 
-let cognome = prompt("Qual è il tuo cognome?")
-console.log(cognome)
+let cognome = prompt("Qual è il tuo cognome?");
+console.log(cognome);
 
-let colore = prompt("Qual è il tuo colore preferito?")
-console.log(colore)
+let colore = prompt("Qual è il tuo colore preferito?");
+console.log(colore);
 
+const numero = 21;
 
+document.getElementById('frase').innerHTML = nome + cognome + colore + numero
